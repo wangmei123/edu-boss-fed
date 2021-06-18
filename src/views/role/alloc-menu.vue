67969653c98b744595b9c250649f6af87edf7675
wangmei <wangmpan0313@163.com>
2021-06-18 17:33:49 +0800

    添加菜单

---
+++ src/views/role/alloc-menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="alloc-menu">
+      <!-- 角色id为：{{ $route.params.roleId}} -->
+      角色id为：{{ roleId}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AllocMenu',
+  props: {
+    roleId: {
+      type: [Number, String],
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/role/components/list.vue
+++ src/views/role/components/list.vue
@@ -15,6 +15,14 @@
                 clearable
               ></el-input>
             </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                @click="onSubmit"
+                v-loading="isLoading"
+              >查询搜索</el-button>
+              <el-button @click="onReset">重置</el-button>
+            </el-form-item>
           </el-form>
         </div>
         <!-- 显示对话框 -->
@@ -32,72 +40,142 @@
           <el-table-column
               prop="name"
               label="角色名称"
+              align="center"
           >
           </el-table-column>
           <el-table-column
               prop="description"
-              label="描述">
+              label="描述"
+              align="center">
           </el-table-column>
           <!-- 设置过滤器需要使用作用域插槽获取数据 -->
           <el-table-column
-              label="添加时间">
+              label="添加时间"
+              align="center">
               <template slot-scope="scope">
                 <span>{{ scope.row.createdTime | dateFormat }}</span>
               </template>
           </el-table-column>
           <el-table-column
-              label="操作">
+              label="操作"
+              align="center">
               <template slot-scope="scope">
+                <div>
                   <el-button
-                  size="mini"
-                  @click="handleEdit(scope.row)"
+                    type="text"
+                    @click="$router.push({
+                      name: 'alloc-menu',
+                      params: {
+                        roleId: scope.row.id
+                      }
+                    })"
                   >分配菜单</el-button>
                   <el-button
-                  size="mini"
-                  @click="handleEdit(scope.row)"
+                    type="text"
                   >分配资源</el-button>
+                </div>
+                <div>
                   <el-button
-                  size="mini"
-                  @click="handleEdit(scope.row)"
+                    type="text"
+                    @click="handleEdit(scope.row)"
                   >编辑</el-button>
                   <el-button
-                  size="mini"
-                  @click="handleDelete(scope.row)"
-                  type="danger"
+                    type="text"
+                    @click="handleDelete(scope.row)"
                   >删除</el-button>
+                </div>
             </template>
           </el-table-column>
         </el-table>
+        <!-- 添加角色的对话框结构 -->
+        <!-- v-if的判断是为了将组件在关闭时销毁，不然组件一直存在，组件的生命周期只会执行一次
+        当需要重复使用create生命周期函数获取一遍数据时，需要卸载这个组件，而不仅仅是不显示，这样就只会请求一次数据，
+        -->
+        <el-dialog
+          :title="isEdit ? '编辑角色' : '添加角色'"
+          :visible.sync="dialogVisible"
+          v-if="dialogVisible"
+          width="30%"
+          >
+          <!-- 将添加与编辑功能单独封装到组件中 -->
+          <create-or-edit
+            :is-edit="isEdit"
+            :role-id="roleId"
+            @success="handleSuccess"
+            @cancel="handleCancel"
+          ></create-or-edit>
+        </el-dialog>
      </el-card>
   </div>
 </template>
 
 <script>
-import { getRoles } from '@/services/role'
+import CreateOrEdit from './CreateOrEdit'
+import { getRoles, deleteRole } from '@/services/role'
 
 export default {
   name: 'RoleList',
+  components: {
+    CreateOrEdit
+  },
   data () {
     return {
       form: {
         name: ''
       },
       roles: [],
-      isLoading: false
+      isLoading: false,
+      // 控制对话框显示
+      dialogVisible: false,
+      // 控制对话框的功能状态
+      isEdit: false,
+      // 存储正在编辑的角色 ID
+      roleId: ''
     }
   },
   created () {
     this.loadRoles()
   },
   methods: {
+    onReset () {},
+    onSubmit () {},
+    // 添加菜单
     handleAdd () {
-
+      this.dialogVisible = true
+      this.isEdit = false
     },
-    handleEdit () {
-
+    // 编辑菜单
+    handleEdit (role) {
+      this.dialogVisible = true
+      this.isEdit = true
+      this.roleId = role.id
     },
-    handleDelete () {
-
+    // 监听子组件的添加状态，成功时触发
+    handleSuccess () {
+      // 隐藏对话框
+      this.dialogVisible = false
+      // 刷新列表
+      this.loadRoles()
+    },
+    // 监听子组件取消状态
+    handleCancel () {
+      this.dialogVisible = false
+    },
+    // 删除角色
+    handleDelete (role) {
+      this.$confirm(`确认删除角色：${role.name}？`, '删除提示')
+        .then(async () => {
+          await deleteRole(role.id)
+          this.$message.success('删除成功')
+          this.loadRoles()
+        })
+        .catch(err => {
+          if (err && err.response) {
+            this.$message.error('删除失败，请重试')
+          } else {
+            this.$message.info('取消删除')
+          }
+        })
     },
     async loadRoles () {
       this.isLoading = true
