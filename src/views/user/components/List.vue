6195716866d20633653e178b6f443b6fea392fc3
wangmei <wangmpan0313@163.com>
2021-06-25 15:17:12 +0800

    分配角色布局

--- src/views/user/components/List.vue
+++ src/views/user/components/List.vue
@@ -90,6 +90,26 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分配角色弹窗 -->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <!-- 下拉菜单 -->
+       <el-select v-model="value1" multiple placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -109,7 +129,25 @@ export default {
         endCreateTime: '',
         rangeDate: []
       },
-      isLoading: false
+      isLoading: false,
+      dialogVisible: false,
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value1: []
     }
   },
   created () {
@@ -146,6 +184,7 @@ export default {
     },
     // 点击用户的分配角色按钮
     handleSelectRole () {
+      this.dialogVisible = true
     }
   }
 }
